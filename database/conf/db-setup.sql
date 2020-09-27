fc40ad029d4a4ee686127f06152d72d625899696
Rongsong Shen <shenrs@cn.ibm.com>
2020-09-27 14:19:22 +0800

    mysql

--- database/conf/db-setup.sql
+++ database/conf/db-setup.sql
@@ -5,7 +5,7 @@ update user  set host='%' where user='root' and host='localhost';
 flush privileges;
 
 {% set password = salt.pillar.get('mysql.root.passwd') %}
-set password for 'root'@'%' = '{{ password }}'
+set password for 'root'@'%' = '{{ password }}' ;
 
 flush privileges;
 
